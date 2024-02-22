e9e973407267836c22590be0e4e59b9455e3109a
HalushkaYura <yuragalyshka@gmail.com>
2024-02-22 19:02:09 +0200

    new project

--- Bramka/Bramka/Shared/WeatherForecast.cs
+++ Bramka/Bramka/Shared/WeatherForecast.cs
@@ -1,4 +1,4 @@
-namespace Bramka.Server
+namespace Bramka.Shared
 {
     public class WeatherForecast
     {
@@ -6,8 +6,8 @@ namespace Bramka.Server
 
         public int TemperatureC { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
         public string? Summary { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
     }
 }
