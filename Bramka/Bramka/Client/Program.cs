1ab1af382d5dd1e6ba1db0191c69cce39a41fac4
Sparty27 <nyshchyi.nazar@gmail.com>
2024-03-02 23:25:31 +0200

    Added CustomAuthStateProvider

--- Bramka/Bramka/Client/Program.cs
+++ Bramka/Bramka/Client/Program.cs
@@ -1,4 +1,6 @@
+using Blazored.LocalStorage;
 using Bramka.Client;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -13,6 +15,9 @@ namespace Bramka.Client
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+            builder.Services.AddAuthorizationCore();
+            builder.Services.AddBlazoredLocalStorage();
 
             await builder.Build().RunAsync();
         }
