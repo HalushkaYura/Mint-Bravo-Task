afaf6e11a28baa3423afaf1d4af8351d8612d290
Sparty27 <nyshchyi.nazar@gmail.com>
2024-03-05 15:27:12 +0200

    Add AuthenticationHandler and update program.cs to use it

--- Bramka/Bramka/Client/Program.cs
+++ Bramka/Bramka/Client/Program.cs
@@ -5,6 +5,10 @@ using Bramka.Client.Services.Interfaces;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Http;
+using Bramka.Client.Handlers;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
 
 namespace Bramka.Client
 {
@@ -16,7 +20,15 @@ namespace Bramka.Client
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
+            builder.Services.AddTransient<AuthenticationHandler>();
+
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddHttpClient("SecuredServer")
+                .ConfigureHttpClient(c => c.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+                .AddHttpMessageHandler<AuthenticationHandler>();
+            builder.Services.RemoveAll<IHttpMessageHandlerBuilderFilter>();
+
+
             builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
             builder.Services.AddScoped<IAuthService, AuthService>();
             builder.Services.AddAuthorizationCore();
