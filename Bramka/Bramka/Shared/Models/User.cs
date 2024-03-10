88d3ab0c35f584da510821f05d1457eeb80a760c
Sparty27 <nyshchyi.nazar@gmail.com>
2024-03-10 04:57:33 +0200

    Update User model

--- Bramka/Bramka/Shared/Models/User.cs
+++ Bramka/Bramka/Shared/Models/User.cs
@@ -6,7 +6,7 @@
         public string Name { get; set; } = string.Empty;
         public string Surname { get; set; } = string.Empty;
         public DateTime BirthDate { get; set; }
-        public string? RefreshToken { get; set; }
+        public string RefreshToken { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string PasswordHash { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; }
