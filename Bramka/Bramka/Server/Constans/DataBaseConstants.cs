2aaa9f41596e1fb7933df9173bd3820a3560248b
Sparty27 <nyshchyi.nazar@gmail.com>
2024-03-11 19:47:30 +0200

    Add SendGrid dependency. Update appsettings.json. Add Model for VerificationCodes table

--- Bramka/Bramka/Server/Constans/DataBaseConstants.cs
+++ Bramka/Bramka/Server/Constans/DataBaseConstants.cs
@@ -58,5 +58,14 @@ namespace Bramka.Server.Constans
 
         #endregion
 
+        #region VerificationCodes
+
+        public const string CreateVerificationCode = "sp_VerificationCodes_CreateCode";
+        public const string GetCodeByUserId = "sp_VerificationCodes_GetCodeByUserId";
+        public const string UpdateVerificationCode = "sp_VerificationCodes_UpdateCode";
+        public const string DeleteVerificationCode = "sp_VerificationCodes_DeleteCode";
+        public const string ConfirmEmail = "sp_VerificationCodes_ConfirmEmail";
+        #endregion
+
     }
 }
